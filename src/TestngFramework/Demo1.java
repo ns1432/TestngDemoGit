f7cce61c317a6e70e439cde9809dbaa6bdadca64
saini <saininidhi@seasia.in>
2021-10-14 13:17:59 +0530

    hello

---
+++ src/TestngFramework/Demo1.java
@@ -0,0 +1,27 @@
+package TestngFramework;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class Demo1 {
+	public static WebDriver driver;
+
+	@BeforeSuite
+	public void setup() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium_material\\chromedriver_win32\\chromedriver.exe");
+		driver =  new ChromeDriver();
+		driver.get("https://www.btes.co.in/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+	}
+	
+	@AfterSuite
+	public void close()
+	{
+		driver.close();
+	}
+
+}
